2f69ef26d86aede825762f3446a0735053868343
EmaTechnologist <emanuele.tufarini@live.com>
2020-09-23 12:36:01 +0000

    commit 23/09/2020_12:36

--- covid19_download_assess.sh
+++ covid19_download_assess.sh
@@ -18,16 +18,20 @@ if echo "$URL";
 fi
 
 
-#vai nella cartella COVID19_DATA
-cd /home/emaserver/DISK1/JUPYTER/COVID19/COVID19_DATA 
-
-#scarica tutti i positivi di ogni provincia
-/home/emaserver/DISK1/JUPYTER/COVID19/BIN/covid_provincia.sh
-
 #seleziona i dati per la provincia di viterbo
 echo "data,stato,codice_regione,denominazione_regione,codice_provincia,denominazione_provincia,sigla_provincia,lat,long,totale_casi,note" > covid19_viterbo.csv
 cat covid19_ita_province.csv | grep VT >> covid19_viterbo.csv
 
+#download andamento nazionale
+URL=$(wget -q --method=HEAD https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv)
+
+if echo "$URL";
+ then
+  wget 'https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale.csv' -O covid19_italia.csv
+  echo "This page exists."
+ else
+  echo "This page does not exist."
+fi
 
 echo ___ all done download, elaboration ...
 
